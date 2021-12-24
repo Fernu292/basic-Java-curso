5f4ab025d295f151cbc663342b5b83d97c194583
Luis Fernando <fernu292@gmail.com>
2021-12-23 21:48:45 -0600

    Comenzando con Java

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args){
+        //Enteros
+        int speed;
+        //Asignar valor a una variable, con dato nombre = valor
+        speed = 10;
+        System.out.println(speed);
+
+        //Strings
+        int salary = 1000;
+        String employerName = "Fernando";
+
+        System.out.println(salary+employerName);
+    }
+}
---
+++ src/updatingVariables.java
@@ -0,0 +1,9 @@
+public class updatingVariables {
+    public static void main(String[] args){
+        int salary = 1000;
+        //Bono de 200
+        salary+=200;
+
+        System.out.println(salary);
+    }
+}
