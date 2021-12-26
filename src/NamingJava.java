7bbeecfac5677c3d34e1a91de7d81c2be20f2185
Luis Fernando <fernu292@gmail.com>
2021-12-26 08:46:46 -0600

    Convencion de variables

---
+++ src/NamingJava.java
@@ -0,0 +1,12 @@
+public class NamingJava {
+    public static void main(String[] args){
+        int celphone = 3333777;
+        System.out.println(celphone);
+
+        String $countryName = "Mexico";
+        String _backgroundColor = "Green";
+
+        String currency$ = "MXN";
+        String backgound_color = "Blue";
+    }
+}
--- src/updatingVariables.java
+++ src/updatingVariables.java
@@ -1,9 +1,28 @@
 public class updatingVariables {
     public static void main(String[] args){
         int salary = 1000;
-        //Bono de 200
+        //Bono de $200
         salary+=200;
-
         System.out.println(salary);
+
+        //pension : $50 de descuento
+        salary-=50;
+        System.out.println(salary);
+
+        //2 horas extra $30 c/u
+        //Comida $45
+        salary+=(30*2)-(45);
+        System.out.println(salary);
+
+        //Actualizando strings, concatenando
+
+        String employeeName = "Luis Fernando";
+        employeeName += " Nuñez";
+
+        System.out.println(employeeName);
+
+        employeeName+= " Rangel";
+        System.out.println("Tu nombre es: "+employeeName);
+
     }
 }
