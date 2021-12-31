5bf39fc80e7e93abab0a0a842fb5603b690277cf
Luis Fernando <fernu292@gmail.com>
2021-12-30 20:49:22 -0600

    Agregando funciones Java

--- Basic/src/IfStatement.java
+++ Basic/src/IfStatement.java
@@ -1,9 +1,39 @@
 public class IfStatement {
     public static void main(String [] args){
         boolean isBluetoothEnabled = true;
-
+        int flieSended = 3;
         if (isBluetoothEnabled){
-            System.out.println("Bluetooth enabled");
+            flieSended++;
+            System.out.println("Archivo enviado");
+        }
+
+        //Operadores Logicos
+
+        //AND
+
+        if(isBluetoothEnabled && flieSended>=3){
+            System.out.println("Bluetooth activado y datos enviados");
+        }
+        //OR
+        if(isBluetoothEnabled || flieSended==3){
+            System.out.println("Bluetooth o archivos enviados");
+        }
+
+        //Switch case
+
+        switch (flieSended){
+            case 1:
+                System.out.println("El numero es 1");
+                break;
+            case 2:
+                System.out.println("El numero es 2");
+                break;
+            case 3:
+                System.out.println("El numero es 3");
+                break;
+            case 4:
+                System.out.println("El numero es 4");
+                break;
         }
     }
 }
